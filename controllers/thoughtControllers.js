fe18cb7f0a3b6aa0b87942624ec25d65e06bbf2c
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-05-04 18:15:13 -0700

    added to thought controllers

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -2,13 +2,17 @@ const {User, Thought} = require('../models');
 
 module.exports = {
     getThoughts(req, res) {
-
+        Thought.find ()
+        .then ((thoughts) => res.status(200).json(thoughts))
+        .catch ((err) => res.status(500).json(err));
     },
     createThought(req, res) {
 
     },
     getSingleThought (req, res) {
-
+        Thought.findById (req.params.id)
+        .then ((thoughts) => res.status(200).json(thoughts))
+        .catch ((err) => res.status(500).json(err));
     },
     updateThought (req, res) {
 
